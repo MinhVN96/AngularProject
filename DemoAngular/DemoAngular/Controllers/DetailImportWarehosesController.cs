79da3e007e1d5cc1273b163a2329e0d275b4dd11
MinhVN <ngocminh0596@gmail.com>
2019-04-04 21:28:41 +0700

    day 4/4

--- DemoAngular/DemoAngular/Controllers/DetailImportWarehosesController.cs
+++ DemoAngular/DemoAngular/Controllers/DetailImportWarehosesController.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using DemoAngular.Models;
 
 namespace DemoAngular.Controllers
 {
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
     public class DetailImportWarehosesController : ApiController
     {
         private WareHouseDB db = new WareHouseDB();
